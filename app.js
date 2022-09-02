a57b95e0c535f942ede6adab9b704d4efc0625e8
Rrk Code <vvs.coder@gmail.com>
2022-09-02 16:07:21 +0300

    Add all files

---
+++ app.js
@@ -0,0 +1,38 @@
+console.log(1 /0 );
+console.log(102 * "fdfs");
+console.log("Hello");
+console.log('Hello');
+console.log(true, false);
+console.log(null);
+console.log(undefined);
+console.table({name: 'Andrey', age: '40'});
+console.log([1, 2, 3, 4, 5, 6]);
+
+// ПЕРЕМЕННЫЕ //
+
+// var
+var name = 'Denis';
+console.log(name);
+name = 'Den';
+console.log(name);
+console.clear();
+
+// let
+let firstName = 'Oleg';
+console.log(firstName);
+
+// const
+const lastName = 'user';
+console.log(lastName);
+
+const myStat = {
+    name: 'Andrew',
+    age: '17'
+};
+console.log(myStat)
+
+
+  
+
+
+
