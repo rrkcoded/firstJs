96b43f44eda3d0d3eeb36186a7ac16c4a774e7ba
Rrk Code <vvs.coder@gmail.com>
2022-09-02 19:50:23 +0300

    Add work with numbers and math

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var name = 'Denis';
 console.log(name);
 name = 'Den';
 console.log(name);
-console.clear();
 
 // let
 let firstName = 'Oleg';
@@ -54,6 +53,45 @@ value = true + 30;
 
 console.log(value)
 console.log(typeof value)
+console.clear();
 
+// Numbers
+
+const num1 = 10;
+const num2 = 20;
+let valuex;
+valuex = num1 + num2;
+valuex = valuex + 100;
+
+console.log(valuex)
+
+//   + * / - %
+value = num1 + num2;
+value = value + 100;
+value += 100;
+// value = 5 % 2;
+value++;
+value--;
+++value;
+--value;
+
+value = 0.6 + 0.7;
+// value = parseFloat(value.toFixed(1));
+value = ( 0.6 * 10 + 0.7 * 10 ) / 10;
+
+// Math
+value = Math.PI; // число пи
+value = Math.random();   // рандом 0-1
+value = Math.round(2.4); // обычное округление
+value = Math.ceil(2.1); // округления в большую пользу
+value = Math.floor(2.9); // округления в меньшую пользу
+value = Math.min(2, 12, 15, 0, 12); //отбиракет мин значение
+
+value = Math.floor(Math.random() * 10 + 1); // рандом 1-10
+
+const arr = ['black', 'red', 'yellow', 'pink', 'white', 'blue', 'orange', 'green'];
+value = Math.floor(Math.random() * arr.length);
+
+console.log(value, arr[value]);
 
 
