bd45ff2174b351bd602b6ccfb47e2781a832bda7
Rrk Code <vvs.coder@gmail.com>
2022-09-05 18:04:05 +0300

    Add a new lines with html

--- newapp.js
+++ newapp.js
@@ -24,4 +24,12 @@ str = `
  <li>5 + 5 = ${ 5+6 }</li>
 </ul>
 `;
+// let price  = 20;
+// let discount = 5;
+// let priceElement = document.getElementById('price');
+// priceElement.innerHTML = getFinalPrice(price, discount);
+// function getFinalPrice(price, discount)
+// {
+//     return price - discount;
+// }
 document.body.innerHTML = str;
